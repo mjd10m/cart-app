374a614096d2744eba18804d0eee32394c0d30c0
mjd10m <mjd10m@outlook.com>
2024-11-22 21:10:54 -0500

    table header component created

---
+++ client/src/assets/components/table-header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function TableHeader() {
+  return (
+    <thead>
+        <tr>
+            <th>Transaction ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>DOB</th>
+            <th>Address 1</th>
+            <th>Address 2</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th># Passengers</th>
+            <th>Color</th>
+            <th>Plate Option</th>
+            <th>Plate Type</th>
+            <th>Plate Num</th>
+        </tr>
+    </thead>
+  )
+}
+
+export default TableHeader
--- client/src/assets/pages/admin/index.jsx
+++ client/src/assets/pages/admin/index.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom'
 import { useQuery, useMutation } from '@apollo/client'
 import {QUERY_CUSTOMERS} from '../../../utils/queries'
 import {GET_SIGNED_URLS, SIGNUP} from '../../../utils/mutations'
-import Topbar from '../../components/topbar';
 import TableRecord from '../../components/table-record'
+import TableHeader from '../../components/table-header';
 
 function AdminPage() {
 
@@ -82,26 +82,7 @@ function AdminPage() {
     return(
         <Container>
             <Table className='mb-3' striped bordered hover responsive>
-            <thead>
-                <tr>
-                    <th>Transaction ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>DOB</th>
-                    <th>Address 1</th>
-                    <th>Address 2</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zip</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th># Passengers</th>
-                    <th>Color</th>
-                    <th>Plate Option</th>
-                    <th>Plate Type</th>
-                    <th>Plate Num</th>
-                </tr>
-            </thead>
+            <TableHeader></TableHeader>
             <tbody>
                 {customers.map((person, index) => (
                     <TableRecord key={index} customerData = {person} handleClick = {handleDownload} index={index}/>
