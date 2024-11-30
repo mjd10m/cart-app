d7c01be6893f6f083933812291353cffb24d2372
mjd10m <mjd10m@outlook.com>
2024-11-30 15:43:08 -0500

    signup reformatted

--- client/src/assets/pages/login/index.jsx
+++ client/src/assets/pages/login/index.jsx
@@ -8,17 +8,13 @@ import { LOGIN } from '../../../utils/mutations'
 import Auth from '../../../utils/auth'
 
 function Login() {
-  const [selectedTab, setSelectedTab] = useState('login')
   const [loginError, setLoginError] = useState({message: ''})
   const [login] = useMutation(LOGIN)
   const {Formik} = formik 
-  const validationSchema = (selectedTab) => {
+  const validationSchema = () => {
     return yup.object().shape({
       username: yup.string().required('Required'),
       password: yup.string().required('Required'),
-      confirmPassword: selectedTab === 'signup'
-        ? yup.string().required('Please enter the password again').oneOf([yup.ref('password'), null], "Passwords didn't match")
-        : yup.string().notRequired(),
     })
   }
   const initialValues = {
@@ -53,23 +49,23 @@ function Login() {
         <Col xs='8'>
           <Card>
             <Card.Header>
-              <Tabs id="card-tabs" activeKey={selectedTab} onSelect={handleTabSelect} className="mb-0 justify-content-center" justify>
-                <Tab eventKey="login" title="Login" />
-              </Tabs>
+              <Row className="justify-content-center">
+                <h3 className='mb-0 text-center'>Login</h3>
+              </Row>
             </Card.Header>
             <Card.Body>
-            <Formik validationSchema={validationSchema(selectedTab)} onSubmit={handleSubmit} initialValues={{...initialValues}} >
+            <Formik validationSchema={validationSchema()} onSubmit={handleSubmit} initialValues={{...initialValues}} >
               {({handleSubmit, handleChange, values, touched, errors  }) => (
               <Form noValidate onSubmit={handleSubmit} className='p-3'>
                 <Form.Group as={Row} className='mb-3' controlId="username">
-                  <Form.Label column xs={5} >Username</Form.Label>
+                  <Form.Label column xs={3} >Username</Form.Label>
                   <Col>
                     <Form.Control type="text" name="username" value = {values.username} onChange={handleChange} isInvalid={touched.username && !!errors.username}/>
                     <Form.Control.Feedback type='invalid'>{errors.username}</Form.Control.Feedback>
                   </Col>
                 </Form.Group>
-                <Form.Group as={Row} className='mb-3' controlId="password">
-                  <Form.Label column xs={5} >Password</Form.Label>
+                <Form.Group as={Row} className='mb-4' controlId="password">
+                  <Form.Label column xs={3} >Password</Form.Label>
                   <Col>
                     <Form.Control type="password" name="password" value = {values.password} onChange={handleChange} isInvalid={touched.password && !!errors.password}/>
                     <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
@@ -86,7 +82,7 @@ function Login() {
                 ):(
                   <></>
                 )}
-                <Row className="justify-content-center mb-3">
+                <Row className="justify-content-center mb-2">
                     <Col xs="4" className='text-center'>
                         <Button className=' w-100' type="submit">
                             Submit
